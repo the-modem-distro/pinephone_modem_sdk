23f6f83bf3834c498d89d2324408057e67a58241
Biktor <biktor@biktorgj.com>
2022-05-15 11:57:53 +0200

    0.6.5: Update licenses for all projects

--- licenses/licenses/elfutils-native/debuginfod-client.c
+++ licenses/licenses/elfutils-native/debuginfod-client.c
@@ -1,5 +1,5 @@
 /* Retrieve ELF / DWARF / source files from the debuginfod.
-   Copyright (C) 2019-2020 Red Hat, Inc.
+   Copyright (C) 2019-2021 Red Hat, Inc.
    This file is part of elfutils.
 
    This file is free software; you can redistribute it and/or modify
